3b319aa35705ca83d2e0c4b4d16b4f40a66f7ef8
Vladimir Shitov <vladimirshitov98@gmail.com>
2021-08-30 00:42:02 +0700

    Comment file to be compatible with python 3.8

---
+++ communiquer/types.py
@@ -0,0 +1,5 @@
+# from typing import Literal
+
+# CPDB_METHOD = Literal["analysis", "statistical_analysis"]
+# GENES_ENCODING = Literal["ensembl", "gene_name", "hgnc_symbol"]
+# CPDB_OUTPUT_FORMAT = Literal["txt", "csv", "tsv", "tab"]
